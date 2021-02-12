6838385810d4c5c60d659bdf820f16d92df383f6
matthenning <pub@matthias-henning.de>
2021-02-12 15:51:38 +0100

    fixed stepper form button alignment

--- src/components/forms/StepperFormStep.vue
+++ src/components/forms/StepperFormStep.vue
@@ -1,14 +1,16 @@
 <template>
     <v-stepper-content :step="step">
         <ValidationObserver :ref="ref" v-slot="{ validate, reset }">
-            <v-card class="mb-12" outlined>
+            <v-card class="mb-12" flat>
                 <v-card-text>
                     <slot> </slot>
                 </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" class="ml-2" type="submit" @click.prevent="onSubmitStep($refs[ref])">Continue</v-btn>
+                    <v-btn v-if="onGoBack" class="ml-2" @click="onGoBack">Back</v-btn>
+                    <v-btn v-if="onSkipToEnd" class="ml-2" @click="onSkipToEnd">Skip to end</v-btn>
+                </v-card-actions>
             </v-card>
-            <v-btn color="primary" class="mpa-1" type="submit" @click.prevent="onSubmitStep($refs[ref])">Continue</v-btn>
-            <v-btn v-if="onGoBack" class="ma-1" @click="onGoBack">Back</v-btn>
-            <v-btn v-if="onSkipToEnd" class="ma-1" @click="onSkipToEnd">Skip to end</v-btn>
         </ValidationObserver>
     </v-stepper-content>
 </template>
